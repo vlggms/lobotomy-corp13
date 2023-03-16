a2521d6d03e4b881cc5a7b846524bc434b620953
Penelope Haze <out.of.p.haze@proton.me>
2023-03-16 08:54:50 -0500

    Make integration tests test facilitytest

--- tools/ci/run_server.sh
+++ tools/ci/run_server.sh
@@ -6,6 +6,8 @@ mkdir ci_test/config
 
 #test config
 cp tools/ci/ci_config.txt ci_test/config/config.txt
+#so that we get the right default map
+cp tools/ci/ci_maps.txt ci_test/config/maps.txt
 
 cd ci_test
 DreamDaemon lobotomy-corp13.dmb -close -trusted -verbose -params "log-directory=ci"
