0e0db5f9c3e205a51c9850125b35314e78f601fb
Lance <ipodmailisepik@gmail.com>
2023-09-20 19:44:41 -0400

    Update AbnormalityQueue.js

--- tgui/packages/tgui/interfaces/AbnormalityQueue.js
+++ tgui/packages/tgui/interfaces/AbnormalityQueue.js
@@ -23,7 +23,7 @@ export const AbnormalityQueue = (props, context) => {
         <Flex direction="column" mb={1}>
           <Section title="Currently queued abnormality" bold>
             <Box as="span" color={data.colorcurrent}>[{threatcurrent}]</Box> {current}
-            <br/>
+            <br />
             <Box as="span" color={data.colorcurrent}>{"PE Difference: "}</Box> {pe_dif}
           </Section>
         </Flex>
