HolyTigerGod/Voidcrew

Refactors offering items, adds cheek kisses (#61590)

code/game/objects/items/hand_items.dm
@@ -102,62 +102,6 @@
 		to_chat(sucker, span_userdanger("[owner] bops you with [owner.p_their()] [src.name]!"))
 	qdel(src)
 
-/obj/item/slapper
-	name = "slapper"
-	desc = "This is how real men fight."
-	icon_state = "latexballon"
-	inhand_icon_state = "nothing"
-	force = 0
-	throwforce = 0
-	item_flags = DROPDEL | ABSTRACT | HAND_ITEM
-	attack_verb_continuous = list("slaps")
-	attack_verb_simple = list("slap")
-	hitsound = 'sound/effects/snap.ogg'
-	/// How many smaller table smacks we can do before we're out
-	var/table_smacks_left = 3
-
-/obj/item/slapper/attack(mob/M, mob/living/carbon/human/user)
-	if(ishuman(M))
-		var/mob/living/carbon/human/L = M
-		if(L && L.dna && L.dna.species)
-			L.dna.species.stop_wagging_tail(M)
-	user.do_attack_animation(M)
-	playsound(M, 'sound/weapons/slap.ogg', 50, TRUE, -1)
-	if(user.zone_selected == BODY_ZONE_HEAD || user.zone_selected == BODY_ZONE_PRECISE_MOUTH)
-		user.visible_message(span_danger("[user] slaps [M] in the face!"),
-		span_notice("You slap [M] in the face!"),\
-		span_hear("You hear a slap."))
-	else
-		user.visible_message(span_danger("[user] slaps [M]!"),
-		span_notice("You slap [M]!"),\
-		span_hear("You hear a slap."))
-	return
-
-/obj/item/slapper/attack_atom(obj/O, mob/living/user, params)
-	if(!istype(O, /obj/structure/table))
-		return ..()
-
-	var/obj/structure/table/the_table = O
-	var/is_right_clicking = LAZYACCESS(params2list(params), RIGHT_CLICK)
-
-	if(is_right_clicking && table_smacks_left == initial(table_smacks_left)) // so you can't do 2 weak slaps followed by a big slam
-		transform = transform.Scale(5) // BIG slap
-		if(HAS_TRAIT(user, TRAIT_HULK))
-			transform = transform.Scale(2)
-			color = COLOR_GREEN
-		user.do_attack_animation(the_table)
-		SEND_SIGNAL(user, COMSIG_LIVING_SLAM_TABLE, the_table)
-		SEND_SIGNAL(the_table, COMSIG_TABLE_SLAMMED, user)
-		playsound(get_turf(the_table), 'sound/effects/tableslam.ogg', 110, TRUE)
-		user.visible_message("<b>[span_danger("[user] slams [user.p_their()] fist down on [the_table]!")]</b>", "<b>[span_danger("You slam your fist down on [the_table]!")]</b>")
-		qdel(src)
-	else
-		user.do_attack_animation(the_table)
-		playsound(get_turf(the_table), 'sound/effects/tableslam.ogg', 40, TRUE)
-		user.visible_message(span_notice("[user] slaps [user.p_their()] hand on [the_table]."), span_notice("You slap your hand on [the_table]."), vision_distance=COMBAT_MESSAGE_RANGE)
-		table_smacks_left--
-		if(table_smacks_left <= 0)
-			qdel(src)
 
 /obj/item/noogie
 	name = "noogie"
@@ -255,6 +199,129 @@
 	noogie_loop(user, target, iteration)
 
 
+/obj/item/slapper
+	name = "slapper"
+	desc = "This is how real men fight."
+	icon_state = "latexballon"
+	inhand_icon_state = "nothing"
+	force = 0
+	throwforce = 0
+	item_flags = DROPDEL | ABSTRACT | HAND_ITEM
+	attack_verb_continuous = list("slaps")
+	attack_verb_simple = list("slap")
+	hitsound = 'sound/effects/snap.ogg'
+	/// How many smaller table smacks we can do before we're out
+	var/table_smacks_left = 3
+
+/obj/item/slapper/attack(mob/living/M, mob/living/carbon/human/user)
+	if(ishuman(M))
+		var/mob/living/carbon/human/L = M
+		if(L && L.dna && L.dna.species)
+			L.dna.species.stop_wagging_tail(M)
+	user.do_attack_animation(M)
+
+	var/slap_volume = 50
+	var/datum/status_effect/offering/kiss_check = M.has_status_effect(STATUS_EFFECT_OFFERING)
+	if(kiss_check && istype(kiss_check.offered_item, /obj/item/kisser) && (user in kiss_check.possible_takers))
+		user.visible_message(span_danger("[user] scoffs at [M]'s advance, winds up, and smacks [M.p_them()] hard to the ground!"),
+			span_notice("The nerve! You wind back your hand and smack [M] hard enough to knock [M.p_them()] over!"),
+			span_hear("You hear someone get the everloving shit smacked out of them!"), ignored_mobs = M)
+		to_chat(M, span_userdanger("You see [user] scoff and pull back [user.p_their()] arm, then suddenly you're on the ground with an ungodly ringing in your ears!"))
+		slap_volume = 120
+		SEND_SOUND(M, sound('sound/weapons/flash_ring.ogg'))
+		shake_camera(M, 2, 2)
+		M.Paralyze(2.5 SECONDS)
+		M.add_confusion(7)
+		M.adjustStaminaLoss(40)
+	else if(user.zone_selected == BODY_ZONE_HEAD || user.zone_selected == BODY_ZONE_PRECISE_MOUTH)
+		user.visible_message(span_danger("[user] slaps [M] in the face!"),
+			span_notice("You slap [M] in the face!"),
+			span_hear("You hear a slap."))
+	else
+		user.visible_message(span_danger("[user] slaps [M]!"),
+			span_notice("You slap [M]!"),
+			span_hear("You hear a slap."))
+	playsound(M, 'sound/weapons/slap.ogg', slap_volume, TRUE, -1)
+	return
+
+/obj/item/slapper/attack_atom(obj/O, mob/living/user, params)
+	if(!istype(O, /obj/structure/table))
+		return ..()
+
+	var/obj/structure/table/the_table = O
+	var/is_right_clicking = LAZYACCESS(params2list(params), RIGHT_CLICK)
+
+	if(is_right_clicking && table_smacks_left == initial(table_smacks_left)) // so you can't do 2 weak slaps followed by a big slam
+		transform = transform.Scale(5) // BIG slap
+		if(HAS_TRAIT(user, TRAIT_HULK))
+			transform = transform.Scale(2)
+			color = COLOR_GREEN
+		user.do_attack_animation(the_table)
+		SEND_SIGNAL(user, COMSIG_LIVING_SLAM_TABLE, the_table)
+		SEND_SIGNAL(the_table, COMSIG_TABLE_SLAMMED, user)
+		playsound(get_turf(the_table), 'sound/effects/tableslam.ogg', 110, TRUE)
+		user.visible_message("<b>[span_danger("[user] slams [user.p_their()] fist down on [the_table]!")]</b>", "<b>[span_danger("You slam your fist down on [the_table]!")]</b>")
+		qdel(src)
+	else
+		user.do_attack_animation(the_table)
+		playsound(get_turf(the_table), 'sound/effects/tableslam.ogg', 40, TRUE)
+		user.visible_message(span_notice("[user] slaps [user.p_their()] hand on [the_table]."), span_notice("You slap your hand on [the_table]."), vision_distance=COMBAT_MESSAGE_RANGE)
+		table_smacks_left--
+		if(table_smacks_left <= 0)
+			qdel(src)
+
+/obj/item/slapper/on_offered(mob/living/carbon/offerer)
+	. = TRUE
+
+	if(!(locate(/mob/living/carbon) in orange(1, offerer)))
+		visible_message(span_danger("[offerer] raises [offerer.p_their()] arm, looking around for a high-five, but there's no one around!"), \
+			span_warning("You post up, looking for a high-five, but finding no one within range!"), null, 2)
+		return
+
+	offerer.visible_message(span_notice("[offerer] raises [offerer.p_their()] arm, looking for a high-five!"), \
+		span_notice("You post up, looking for a high-five!"), null, 2)
+	offerer.apply_status_effect(STATUS_EFFECT_OFFERING, src, /atom/movable/screen/alert/give/highfive)
+
+/// Yeah broh! This is where we do the high-fiving (or high-tenning :o)
+/obj/item/slapper/on_offer_taken(mob/living/carbon/offerer, mob/living/carbon/taker)
+	. = TRUE
+
+	var/open_hands_taker
+	var/slappers_giver
+	for(var/i in taker.held_items) // see how many hands the taker has open for high'ing
+		if(isnull(i))
+			open_hands_taker++
+
+	if(!open_hands_taker)
+		to_chat(taker, span_warning("You can't high-five [offerer] with no open hands!"))
+		SEND_SIGNAL(taker, COMSIG_ADD_MOOD_EVENT, "high_five", /datum/mood_event/high_five_full_hand) // not so successful now!
+		return
+
+	for(var/i in offerer.held_items)
+		var/obj/item/slapper/slap_check = i
+		if(istype(slap_check))
+			slappers_giver++
+
+	if(slappers_giver >= 2) // we only check this if it's already established the taker has 2+ hands free
+		offerer.visible_message(span_notice("[taker] enthusiastically high-tens [offerer]!"), span_nicegreen("Wow! You're high-tenned [taker]!"), span_hear("You hear a sickening sound of flesh hitting flesh!"), ignored_mobs=taker)
+		to_chat(taker, span_nicegreen("You give high-tenning [offerer] your all!"))
+		playsound(offerer, 'sound/weapons/slap.ogg', 100, TRUE, 1)
+		offerer.mind.add_memory(MEMORY_HIGH_FIVE, list(DETAIL_PROTAGONIST = taker, DETAIL_HIGHFIVE_TYPE = "high ten"), story_value = STORY_VALUE_OKAY)
+		taker.mind.add_memory(MEMORY_HIGH_FIVE, list(DETAIL_PROTAGONIST = offerer, DETAIL_HIGHFIVE_TYPE = "high ten"), story_value = STORY_VALUE_OKAY)
+		SEND_SIGNAL(offerer, COMSIG_ADD_MOOD_EVENT, "high_five", /datum/mood_event/high_ten)
+		SEND_SIGNAL(taker, COMSIG_ADD_MOOD_EVENT, "high_five", /datum/mood_event/high_ten)
+	else
+		offerer.visible_message(span_notice("[taker] high-fives [offerer]!"), span_nicegreen("All right! You're high-fived by [taker]!"), span_hear("You hear a sickening sound of flesh hitting flesh!"), ignored_mobs=taker)
+		to_chat(taker, span_nicegreen("You high-five [offerer]!"))
+		playsound(offerer, 'sound/weapons/slap.ogg', 50, TRUE, -1)
+		offerer.mind.add_memory(MEMORY_HIGH_FIVE, list(DETAIL_PROTAGONIST = taker, DETAIL_HIGHFIVE_TYPE = "high five"), story_value = STORY_VALUE_OKAY)
+		taker.mind.add_memory(MEMORY_HIGH_FIVE, list(DETAIL_PROTAGONIST = offerer, DETAIL_HIGHFIVE_TYPE = "high five"), story_value = STORY_VALUE_OKAY)
+		SEND_SIGNAL(offerer, COMSIG_ADD_MOOD_EVENT, "high_five", /datum/mood_event/high_five)
+		SEND_SIGNAL(taker, COMSIG_ADD_MOOD_EVENT, "high_five", /datum/mood_event/high_five)
+	qdel(src)
+
+
+
 /obj/item/kisser
 	name = "kiss"
 	desc = "I want you all to know, everyone and anyone, to seal it with a kiss."
@@ -266,7 +333,8 @@
 	item_flags = DROPDEL | ABSTRACT | HAND_ITEM
 	/// The kind of projectile this version of the kiss blower fires
 	var/kiss_type = /obj/projectile/kiss
-
+	/// TRUE if the user was aiming anywhere but the mouth when they offer the kiss, if it's offered
+	var/cheek_kiss
 
 /obj/item/kisser/afterattack(atom/target, mob/user, proximity_flag, click_parameters)
 	. = ..()
@@ -276,7 +344,6 @@
 	user.visible_message("<b>[user]</b> blows \a [blown_kiss] at [target]!", span_notice("You blow \a [blown_kiss] at [target]!"))
 
 	//Shooting Code:
-	blown_kiss.spread = 0
 	blown_kiss.original = target
 	blown_kiss.fired_from = user
 	blown_kiss.firer = user // don't hit ourself that would be really annoying
@@ -284,6 +351,34 @@
 	blown_kiss.preparePixelProjectile(target, user)
 	blown_kiss.fire()
 	qdel(src)
+
+/obj/item/kisser/on_offered(mob/living/carbon/offerer)
+	if(!(locate(/mob/living/carbon) in orange(1, offerer)))
+		return TRUE
+
+	cheek_kiss = (offerer.zone_selected != BODY_ZONE_PRECISE_MOUTH)
+	offerer.visible_message(span_notice("[offerer] leans in slightly, offering a kiss[cheek_kiss ? " on the cheek" : ""]!"),
+		span_notice("You lean in slightly, indicating you'd like to offer a kiss[cheek_kiss ? " on the cheek" : ""]!"), null, 2)
+	offerer.apply_status_effect(STATUS_EFFECT_OFFERING, src)
+	return TRUE
+
+/obj/item/kisser/on_offer_taken(mob/living/carbon/offerer, mob/living/carbon/taker)
+	var/obj/projectile/blown_kiss = new kiss_type(get_turf(offerer))
+	offerer.visible_message("<b>[offerer]</b> gives [taker] \a [blown_kiss][cheek_kiss ? " on the cheek" : ""]!!", span_notice("You give [taker] \a [blown_kiss][cheek_kiss ? " on the cheek" : ""]!"), ignored_mobs = taker)
+	to_chat(taker, span_nicegreen("[offerer] gives you \a [blown_kiss][cheek_kiss ? " on the cheek" : ""]!"))
+	offerer.face_atom(taker)
+	taker.face_atom(offerer)
+	offerer.do_item_attack_animation(taker, used_item=src)
+	//We're still firing a shot here because I don't want to deal with some weird edgecase where direct impacting them with the projectile causes it to freak out because there's no angle or something
+	blown_kiss.original = taker
+	blown_kiss.fired_from = offerer
+	blown_kiss.firer = offerer // don't hit ourself that would be really annoying
+	blown_kiss.impacted = list(offerer = TRUE) // just to make sure we don't hit the wearer
+	blown_kiss.preparePixelProjectile(taker, offerer)
+	blown_kiss.suppressed = SUPPRESSED_VERY // this also means it's a direct offer
+	blown_kiss.fire()
+	qdel(src)
+	return TRUE // so the core offering code knows to halt
 
 /obj/item/kisser/death
 	name = "kiss of death"
@@ -325,8 +420,11 @@
  */
 /obj/projectile/kiss/proc/harmless_on_hit(mob/living/living_target)
 	playsound(get_turf(living_target), hitsound, 100, TRUE)
-	living_target.visible_message(span_danger("[living_target] is hit by \a [src]."), span_userdanger("You're hit by \a [src]!"), vision_distance=COMBAT_MESSAGE_RANGE)
-	living_target.mind?.add_memory(MEMORY_KISS, list(DETAIL_PROTAGONIST = living_target, DETAIL_KISSER = firer), story_value = STORY_VALUE_OKAY, memory_flags = MEMORY_CHECK_BLINDNESS)
+	if(!suppressed)  // direct
+		living_target.visible_message(span_danger("[living_target] is hit by \a [src]."), span_userdanger("You're hit by \a [src]!"), vision_distance=COMBAT_MESSAGE_RANGE)
+
+	living_target.mind?.add_memory(MEMORY_KISS, list(DETAIL_PROTAGONIST = living_target, DETAIL_KISSER = firer), story_value = STORY_VALUE_OKAY)
+	SEND_SIGNAL(living_target, COMSIG_ADD_MOOD_EVENT, "kiss", /datum/mood_event/kiss, firer, suppressed)
 	if(isliving(firer))
 		var/mob/living/kisser = firer
 		kisser.mind?.add_memory(MEMORY_KISS, list(DETAIL_PROTAGONIST = living_target, DETAIL_KISSER = firer), story_value = STORY_VALUE_OKAY, memory_flags = MEMORY_CHECK_BLINDNESS)
@@ -364,6 +462,7 @@
 	def_zone = BODY_ZONE_HEAD // let's keep it PG, people
 	. = ..()
 	if(isliving(target))
+		SEND_SIGNAL(target, COMSIG_ADD_MOOD_EVENT, "kiss", /datum/mood_event/kiss, firer, suppressed)
 		try_fluster(target)
 
 /obj/projectile/kiss/death

vlggms/lobotomy-corp13

code/game/objects/items/hand_items.dm
@@ -102,64 +102,6 @@
 		to_chat(sucker, span_userdanger("[owner] bops you with [owner.p_their()] [src.name]!"))
 	qdel(src)
 
-/obj/item/slapper
-	name = "slapper"
-	desc = "This is how real men fight."
-	icon_state = "latexballon"
-	inhand_icon_state = "nothing"
-	force = 0
-	throwforce = 0
-	item_flags = DROPDEL | ABSTRACT | HAND_ITEM
-	attack_verb_continuous = list("slaps")
-	attack_verb_simple = list("slap")
-	hitsound = 'sound/effects/snap.ogg'
-	/// How many smaller table smacks we can do before we're out
-	var/table_smacks_left = 3
-
-/obj/item/slapper/attack(mob/M, mob/living/carbon/human/user)
-	if(ishuman(M))
-		var/mob/living/carbon/human/L = M
-		if(L && L.dna && L.dna.species)
-			L.dna.species.stop_wagging_tail(M)
-	user.do_attack_animation(M)
-	playsound(M, 'sound/weapons/slap.ogg', 50, TRUE, -1)
-	if(user.zone_selected == BODY_ZONE_HEAD || user.zone_selected == BODY_ZONE_PRECISE_MOUTH)
-		user.visible_message(span_danger("[user] slaps [M] in the face!"),
-		span_notice("You slap [M] in the face!"),\
-		span_hear("You hear a slap."))
-	else
-		user.visible_message(span_danger("[user] slaps [M]!"),
-		span_notice("You slap [M]!"),\
-		span_hear("You hear a slap."))
-	return
-
-//obj/item/slapper/attack_atom(obj/O, mob/living/user, params) - REQUIRES PR #61183
-/obj/item/slapper/attack_obj(obj/O, mob/living/user, params)
-	if(!istype(O, /obj/structure/table))
-		return ..()
-
-	var/obj/structure/table/the_table = O
-//	var/is_right_clicking = LAZYACCESS(params2list(params), RIGHT_CLICK) LOBOTOMYCORPORATION EDIT REMOVAL - we dont use combat mode
-
-//	if(is_right_clicking && table_smacks_left == initial(table_smacks_left)) // so you can't do 2 weak slaps followed by a big slam // LOBOTOMYCORPORATION EDIT CHANGE OLD - we dont use combat mode
-	if((user.a_intent == INTENT_HARM) && table_smacks_left == initial(table_smacks_left)) // LOBOTOMYCORPORATION EDIT CHANGE NEW - we dont use combat mode
-		transform = transform.Scale(5) // BIG slap
-		if(HAS_TRAIT(user, TRAIT_HULK))
-			transform = transform.Scale(2)
-			color = COLOR_GREEN
-		user.do_attack_animation(the_table)
-		SEND_SIGNAL(user, COMSIG_LIVING_SLAM_TABLE, the_table)
-		SEND_SIGNAL(the_table, COMSIG_TABLE_SLAMMED, user)
-		playsound(get_turf(the_table), 'sound/effects/tableslam.ogg', 110, TRUE)
-		user.visible_message("<b>[span_danger("[user] slams [user.p_their()] fist down on [the_table]!")]</b>", "<b>[span_danger("You slam your fist down on [the_table]!")]</b>")
-		qdel(src)
-	else
-		user.do_attack_animation(the_table)
-		playsound(get_turf(the_table), 'sound/effects/tableslam.ogg', 40, TRUE)
-		user.visible_message(span_notice("[user] slaps [user.p_their()] hand on [the_table]."), span_notice("You slap your hand on [the_table]."), vision_distance=COMBAT_MESSAGE_RANGE)
-		table_smacks_left--
-		if(table_smacks_left <= 0)
-			qdel(src)
 
 /obj/item/noogie
 	name = "noogie"
@@ -188,7 +130,7 @@
 		return
 
 	// [user] gives [target] a [prefix_desc] noogie[affix_desc]!
-/* LOBOTOMYCORPORATION EDIT REMOVAL - PR #1070 - Removes mood
+/* LOBOTOMYCORPORATION REMOVAL - PR #1070 - Removes mood
 	var/brutal_noogie = FALSE // was it an extra hard noogie?
 */
 	var/prefix_desc = "rough"
@@ -199,12 +141,12 @@
 		prefix_desc = "violent"
 		affix_desc = "on [target.p_their()] sensitive antennae"
 		affix_desc_target = "on your highly sensitive antennae"
-/* LOBOTOMYCORPORATION EDIT REMOVAL - PR #1070 - Removes mood
+/* LOBOTOMYCORPORATION REMOVAL - PR #1070 - Removes mood
 		brutal_noogie = TRUE
 */
 	if(user.dna?.check_mutation(HULK))
 		prefix_desc = "sickeningly brutal"
-/* LOBOTOMYCORPORATION EDIT REMOVAL - PR #1070 - Removes mood
+/* LOBOTOMYCORPORATION REMOVAL - PR #1070 - Removes mood
 		brutal_noogie = TRUE
 */
 
@@ -219,7 +161,7 @@
 		to_chat(target, span_danger("[user] fails to give you a noogie!"))
 		return
 
-/* LOBOTOMYCORPORATION EDIT REMOVAL - PR #1070 - Removes mood
+/* LOBOTOMYCORPORATION REMOVAL - PR #1070 - Removes mood
 	if(brutal_noogie)
 		SEND_SIGNAL(target, COMSIG_ADD_MOOD_EVENT, "noogie_harsh", /datum/mood_event/noogie_harsh)
 	else
@@ -265,6 +207,141 @@
 	noogie_loop(user, target, iteration)
 
 
+/obj/item/slapper
+	name = "slapper"
+	desc = "This is how real men fight."
+	icon_state = "latexballon"
+	inhand_icon_state = "nothing"
+	force = 0
+	throwforce = 0
+	item_flags = DROPDEL | ABSTRACT | HAND_ITEM
+	attack_verb_continuous = list("slaps")
+	attack_verb_simple = list("slap")
+	hitsound = 'sound/effects/snap.ogg'
+	/// How many smaller table smacks we can do before we're out
+	var/table_smacks_left = 3
+
+/obj/item/slapper/attack(mob/living/M, mob/living/carbon/human/user)
+	if(ishuman(M))
+		var/mob/living/carbon/human/L = M
+		if(L && L.dna && L.dna.species)
+			L.dna.species.stop_wagging_tail(M)
+	user.do_attack_animation(M)
+
+	var/slap_volume = 50
+	var/datum/status_effect/offering/kiss_check = M.has_status_effect(STATUS_EFFECT_OFFERING)
+	if(kiss_check && istype(kiss_check.offered_item, /obj/item/kisser) && (user in kiss_check.possible_takers))
+		user.visible_message(span_danger("[user] scoffs at [M]'s advance, winds up, and smacks [M.p_them()] hard to the ground!"),
+			span_notice("The nerve! You wind back your hand and smack [M] hard enough to knock [M.p_them()] over!"),
+			span_hear("You hear someone get the everloving shit smacked out of them!"), ignored_mobs = M)
+		to_chat(M, span_userdanger("You see [user] scoff and pull back [user.p_their()] arm, then suddenly you're on the ground with an ungodly ringing in your ears!"))
+		slap_volume = 120
+		SEND_SOUND(M, sound('sound/weapons/flash_ring.ogg'))
+		shake_camera(M, 2, 2)
+		M.Paralyze(2.5 SECONDS)
+		M.add_confusion(7)
+		M.adjustStaminaLoss(40)
+	else if(user.zone_selected == BODY_ZONE_HEAD || user.zone_selected == BODY_ZONE_PRECISE_MOUTH)
+		user.visible_message(span_danger("[user] slaps [M] in the face!"),
+			span_notice("You slap [M] in the face!"),
+			span_hear("You hear a slap."))
+	else
+		user.visible_message(span_danger("[user] slaps [M]!"),
+			span_notice("You slap [M]!"),
+			span_hear("You hear a slap."))
+	playsound(M, 'sound/weapons/slap.ogg', slap_volume, TRUE, -1)
+	return
+
+//obj/item/slapper/attack_atom(obj/O, mob/living/user, params) - REQUIRES PR #61183
+/obj/item/slapper/attack_obj(obj/O, mob/living/user, params)
+	if(!istype(O, /obj/structure/table))
+		return ..()
+
+	var/obj/structure/table/the_table = O
+//	var/is_right_clicking = LAZYACCESS(params2list(params), RIGHT_CLICK) LOBOTOMYCORPORATION REMOVAL - we dont use combat mode
+
+//	if(is_right_clicking && table_smacks_left == initial(table_smacks_left)) // so you can't do 2 weak slaps followed by a big slam // LOBOTOMYCORPORATION EDIT CHANGE OLD - we dont use combat mode
+	if((user.a_intent == INTENT_HARM) && table_smacks_left == initial(table_smacks_left)) // LOBOTOMYCORPORATION EDIT CHANGE NEW - we dont use combat mode
+		transform = transform.Scale(5) // BIG slap
+		if(HAS_TRAIT(user, TRAIT_HULK))
+			transform = transform.Scale(2)
+			color = COLOR_GREEN
+		user.do_attack_animation(the_table)
+		SEND_SIGNAL(user, COMSIG_LIVING_SLAM_TABLE, the_table)
+		SEND_SIGNAL(the_table, COMSIG_TABLE_SLAMMED, user)
+		playsound(get_turf(the_table), 'sound/effects/tableslam.ogg', 110, TRUE)
+		user.visible_message("<b>[span_danger("[user] slams [user.p_their()] fist down on [the_table]!")]</b>", "<b>[span_danger("You slam your fist down on [the_table]!")]</b>")
+		qdel(src)
+	else
+		user.do_attack_animation(the_table)
+		playsound(get_turf(the_table), 'sound/effects/tableslam.ogg', 40, TRUE)
+		user.visible_message(span_notice("[user] slaps [user.p_their()] hand on [the_table]."), span_notice("You slap your hand on [the_table]."), vision_distance=COMBAT_MESSAGE_RANGE)
+		table_smacks_left--
+		if(table_smacks_left <= 0)
+			qdel(src)
+
+/obj/item/slapper/on_offered(mob/living/carbon/offerer)
+	. = TRUE
+
+	if(!(locate(/mob/living/carbon) in orange(1, offerer)))
+		visible_message(span_danger("[offerer] raises [offerer.p_their()] arm, looking around for a high-five, but there's no one around!"), \
+			span_warning("You post up, looking for a high-five, but finding no one within range!"), null, 2)
+		return
+
+	offerer.visible_message(span_notice("[offerer] raises [offerer.p_their()] arm, looking for a high-five!"), \
+		span_notice("You post up, looking for a high-five!"), null, 2)
+	offerer.apply_status_effect(STATUS_EFFECT_OFFERING, src, /atom/movable/screen/alert/give/highfive)
+
+/// Yeah broh! This is where we do the high-fiving (or high-tenning :o)
+/obj/item/slapper/on_offer_taken(mob/living/carbon/offerer, mob/living/carbon/taker)
+	. = TRUE
+
+	var/open_hands_taker
+	var/slappers_giver
+	for(var/i in taker.held_items) // see how many hands the taker has open for high'ing
+		if(isnull(i))
+			open_hands_taker++
+
+	if(!open_hands_taker)
+		to_chat(taker, span_warning("You can't high-five [offerer] with no open hands!"))
+/*  LOBOTOMYCORPORATION REMOVAL - PR #1070 - Removes mood
+		SEND_SIGNAL(taker, COMSIG_ADD_MOOD_EVENT, "high_five", /datum/mood_event/high_five_full_hand) // not so successful now!
+*/
+		return
+
+	for(var/i in offerer.held_items)
+		var/obj/item/slapper/slap_check = i
+		if(istype(slap_check))
+			slappers_giver++
+
+	if(slappers_giver >= 2) // we only check this if it's already established the taker has 2+ hands free
+		offerer.visible_message(span_notice("[taker] enthusiastically high-tens [offerer]!"), span_nicegreen("Wow! You're high-tenned [taker]!"), span_hear("You hear a sickening sound of flesh hitting flesh!"), ignored_mobs=taker)
+		to_chat(taker, span_nicegreen("You give high-tenning [offerer] your all!"))
+		playsound(offerer, 'sound/weapons/slap.ogg', 100, TRUE, 1)
+/* LOBOTOMYCORPORATION REMOVAL - We dont have memories
+		offerer.mind.add_memory(MEMORY_HIGH_FIVE, list(DETAIL_PROTAGONIST = taker, DETAIL_HIGHFIVE_TYPE = "high ten"), story_value = STORY_VALUE_OKAY)
+		taker.mind.add_memory(MEMORY_HIGH_FIVE, list(DETAIL_PROTAGONIST = offerer, DETAIL_HIGHFIVE_TYPE = "high ten"), story_value = STORY_VALUE_OKAY)
+*/
+/* LOBOTOMYCORPORATION REMOVAL - PR #1070 - Removes mood
+		SEND_SIGNAL(offerer, COMSIG_ADD_MOOD_EVENT, "high_five", /datum/mood_event/high_ten)
+		SEND_SIGNAL(taker, COMSIG_ADD_MOOD_EVENT, "high_five", /datum/mood_event/high_ten)
+*/
+	else
+		offerer.visible_message(span_notice("[taker] high-fives [offerer]!"), span_nicegreen("All right! You're high-fived by [taker]!"), span_hear("You hear a sickening sound of flesh hitting flesh!"), ignored_mobs=taker)
+		to_chat(taker, span_nicegreen("You high-five [offerer]!"))
+		playsound(offerer, 'sound/weapons/slap.ogg', 50, TRUE, -1)
+/* LOBOTOMYCORPORATION REMOVAL - We dont have memories
+		offerer.mind.add_memory(MEMORY_HIGH_FIVE, list(DETAIL_PROTAGONIST = taker, DETAIL_HIGHFIVE_TYPE = "high five"), story_value = STORY_VALUE_OKAY)
+		taker.mind.add_memory(MEMORY_HIGH_FIVE, list(DETAIL_PROTAGONIST = offerer, DETAIL_HIGHFIVE_TYPE = "high five"), story_value = STORY_VALUE_OKAY)
+*/
+/* LOBOTOMYCORPORATION REMOVAL - PR #1070 - Removes mood
+		SEND_SIGNAL(offerer, COMSIG_ADD_MOOD_EVENT, "high_five", /datum/mood_event/high_five)
+		SEND_SIGNAL(taker, COMSIG_ADD_MOOD_EVENT, "high_five", /datum/mood_event/high_five)
+*/
+	qdel(src)
+
+
+
 /obj/item/kisser
 	name = "kiss"
 	desc = "I want you all to know, everyone and anyone, to seal it with a kiss."
@@ -276,7 +353,8 @@
 	item_flags = DROPDEL | ABSTRACT | HAND_ITEM
 	/// The kind of projectile this version of the kiss blower fires
 	var/kiss_type = /obj/projectile/kiss
-
+	/// TRUE if the user was aiming anywhere but the mouth when they offer the kiss, if it's offered
+	var/cheek_kiss
 
 /obj/item/kisser/afterattack(atom/target, mob/user, proximity_flag, click_parameters)
 	. = ..()
@@ -286,7 +364,6 @@
 	user.visible_message("<b>[user]</b> blows \a [blown_kiss] at [target]!", span_notice("You blow \a [blown_kiss] at [target]!"))
 
 	//Shooting Code:
-	blown_kiss.spread = 0
 	blown_kiss.original = target
 	blown_kiss.fired_from = user
 	blown_kiss.firer = user // don't hit ourself that would be really annoying
@@ -294,6 +371,34 @@
 	blown_kiss.preparePixelProjectile(target, user)
 	blown_kiss.fire()
 	qdel(src)
+
+/obj/item/kisser/on_offered(mob/living/carbon/offerer)
+	if(!(locate(/mob/living/carbon) in orange(1, offerer)))
+		return TRUE
+
+	cheek_kiss = (offerer.zone_selected != BODY_ZONE_PRECISE_MOUTH)
+	offerer.visible_message(span_notice("[offerer] leans in slightly, offering a kiss[cheek_kiss ? " on the cheek" : ""]!"),
+		span_notice("You lean in slightly, indicating you'd like to offer a kiss[cheek_kiss ? " on the cheek" : ""]!"), null, 2)
+	offerer.apply_status_effect(STATUS_EFFECT_OFFERING, src)
+	return TRUE
+
+/obj/item/kisser/on_offer_taken(mob/living/carbon/offerer, mob/living/carbon/taker)
+	var/obj/projectile/blown_kiss = new kiss_type(get_turf(offerer))
+	offerer.visible_message("<b>[offerer]</b> gives [taker] \a [blown_kiss][cheek_kiss ? " on the cheek" : ""]!!", span_notice("You give [taker] \a [blown_kiss][cheek_kiss ? " on the cheek" : ""]!"), ignored_mobs = taker)
+	to_chat(taker, span_nicegreen("[offerer] gives you \a [blown_kiss][cheek_kiss ? " on the cheek" : ""]!"))
+	offerer.face_atom(taker)
+	taker.face_atom(offerer)
+	offerer.do_item_attack_animation(taker, used_item=src)
+	//We're still firing a shot here because I don't want to deal with some weird edgecase where direct impacting them with the projectile causes it to freak out because there's no angle or something
+	blown_kiss.original = taker
+	blown_kiss.fired_from = offerer
+	blown_kiss.firer = offerer // don't hit ourself that would be really annoying
+	blown_kiss.impacted = list(offerer = TRUE) // just to make sure we don't hit the wearer
+	blown_kiss.preparePixelProjectile(taker, offerer)
+	blown_kiss.suppressed = SUPPRESSED_VERY // this also means it's a direct offer
+	blown_kiss.fire()
+	qdel(src)
+	return TRUE // so the core offering code knows to halt
 
 /obj/item/kisser/death
 	name = "kiss of death"
@@ -334,9 +439,11 @@
  */
 /obj/projectile/kiss/proc/harmless_on_hit(mob/living/living_target)
 	playsound(get_turf(living_target), hitsound, 100, TRUE)
-	living_target.visible_message(span_danger("[living_target] is hit by \a [src]."), span_userdanger("You're hit by \a [src]!"), vision_distance=COMBAT_MESSAGE_RANGE)
-/* LOBOTOMYCORPORATION EDIT REMOVAL - Removes mood
-	living_target.mind?.add_memory(MEMORY_KISS, list(DETAIL_PROTAGONIST = living_target, DETAIL_KISSER = firer), story_value = STORY_VALUE_OKAY, memory_flags = MEMORY_CHECK_BLINDNESS)
+	if(!suppressed)  // direct
+		living_target.visible_message(span_danger("[living_target] is hit by \a [src]."), span_userdanger("You're hit by \a [src]!"), vision_distance=COMBAT_MESSAGE_RANGE)
+/*  LOBOTOMYCORPORATION REMOVAL - PR #1070 - Removes mood
+	living_target.mind?.add_memory(MEMORY_KISS, list(DETAIL_PROTAGONIST = living_target, DETAIL_KISSER = firer), story_value = STORY_VALUE_OKAY)
+	SEND_SIGNAL(living_target, COMSIG_ADD_MOOD_EVENT, "kiss", /datum/mood_event/kiss, firer, suppressed)
 	if(isliving(firer))
 		var/mob/living/kisser = firer
 		kisser.mind?.add_memory(MEMORY_KISS, list(DETAIL_PROTAGONIST = living_target, DETAIL_KISSER = firer), story_value = STORY_VALUE_OKAY, memory_flags = MEMORY_CHECK_BLINDNESS)
@@ -375,6 +482,9 @@
 	def_zone = BODY_ZONE_HEAD // let's keep it PG, people
 	. = ..()
 	if(isliving(target))
+/* LOBOTOMYCORPORATION REMOVAL - PR #1070 - Removes mood
+		SEND_SIGNAL(target, COMSIG_ADD_MOOD_EVENT, "kiss", /datum/mood_event/kiss, firer, suppressed)
+*/
 		try_fluster(target)
 
 /obj/projectile/kiss/death