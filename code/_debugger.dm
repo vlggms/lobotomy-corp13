HolyTigerGod/Voidcrew

The Failsafe can now recover from an deleted MC (#60846)
* The Failsafe can now recover from an deleted MC
Its also more reliable and can handle a situation where its main Loop runtimes and the MC is stuck

* Reset defcon level correctly
Oops left that in from debugging the levels

* Correctly recover SSasset

* Only decrease defcon if MC creation failed
Also add some sort sleep between emergency loops

* Makes the last two emergency actions manual procs
Since they are kinda unstantable its probalby best
if only admins call these manually

Its also more reliable and can handle a situation where its main Loop runtimes and the MC is stuck
You can also now debug Master/New()

While there will most likely never be any situation where the MC is just gone its still good to know that the game can recover from such a situation
For example maybe someone messed up a SDQL query or maybe someone wanted to delete the MC to create a new one hoping the Failsafe would do so for him

code/_debugger.dm
@@ -1,0 +1,13 @@
+//Datum used to init Auxtools debugging as early as possible
+//Datum gets created in master.dm because for whatever reason global code in there gets runs first
+//In case we ever figure out how to manipulate global init order please move the datum creation into this file
+/datum/debugger
+
+/datum/debugger/New()
+		enable_debugger()
+
+/datum/debugger/proc/enable_debugger()
+	var/dll = world.GetConfig("env", "AUXTOOLS_DEBUG_DLL")
+	if (dll)
+		call(dll, "auxtools_init")()
+		enable_debugging()

vlggms/lobotomy-corp13

code/_debugger.dm
@@ -1,0 +1,13 @@
+//Datum used to init Auxtools debugging as early as possible
+//Datum gets created in master.dm because for whatever reason global code in there gets runs first
+//In case we ever figure out how to manipulate global init order please move the datum creation into this file
+/datum/debugger
+
+/datum/debugger/New()
+		enable_debugger()
+
+/datum/debugger/proc/enable_debugger()
+	var/dll = world.GetConfig("env", "AUXTOOLS_DEBUG_DLL")
+	if (dll)
+		LIBCALL(dll, "auxtools_init")()
+		enable_debugging()