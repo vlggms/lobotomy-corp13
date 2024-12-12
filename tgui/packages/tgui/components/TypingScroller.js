a1c22380b2be9c047ceefd7cd8897e5d3de622e9
Anton Zenkov <azenkov@gmail.com>
2024-12-11 21:43:50 -0500

    Scene manager

--- tgui/packages/tgui/components/TypingScroller.js
+++ tgui/packages/tgui/components/TypingScroller.js
@@ -5,20 +5,25 @@ export class TypingScroller extends Component {
     super(props);
     this.containerEl = null;
     this.interval = null;
-    this.state = {
-      displayed: '',
-    };
+    this.state = { displayed: '' };
   }
 
   componentDidMount() {
-    const { text, speed = 100 } = this.props;
+    const { text, speed = 100, onTypingStart, onTypingEnd } = this.props;
     let i = 0;
+    if (text && text.length > 0 && onTypingStart) {
+      onTypingStart();
+    }
+
     this.interval = setInterval(() => {
       if (i < text.length) {
         this.setState(prevState => ({ displayed: prevState.displayed + text[i] }));
         i++;
       } else {
         clearInterval(this.interval);
+        if (onTypingEnd) {
+          onTypingEnd();
+        }
       }
     }, speed);
   }
@@ -37,7 +42,12 @@ export class TypingScroller extends Component {
     return (
       <div
         ref={el => this.containerEl = el}
-        style={{ height: "100px", overflow: "auto", border: "1px solid #ccc" }}
+        style={{
+          height: "192px",
+          overflow: "auto",
+          border: "1px solid #ccc",
+          width: "100%"
+        }}
       >
         {this.state.displayed}
       </div>
--- tgui/packages/tgui/interfaces/SpeakingNpc.js
+++ tgui/packages/tgui/interfaces/SpeakingNpc.js
@@ -1,24 +1,52 @@
 import { useBackend } from "../backend";
-import { Box, Button, LabeledList, Section } from "../components";
+import { Box, Button, LabeledList, Flex, Section } from "../components";
 import { Window } from "../layouts";
 import { TypingScroller } from "../components";
 
 export const SpeakingNpc = (props, context) => {
   const { act, data } = useBackend(context);
-  const { title = "", text = "" } = data || {};
+  const { title = "", text = "", img_url = "", actions = {} } = data || {};
   return (
     <Window title={title}>
       <Window.Content>
         <Section>
-          <TypingScroller text={text} speed={100} />
-          <LabeledList>
-            <LabeledList.Item label="Actions">
-              <Button onClick={() => act("talk")}>Talk</Button>
-              <Button onClick={() => act("pet")}>Pet</Button>
-              <Button onClick={() => act("close")}>Close</Button>
-            </LabeledList.Item>
-          </LabeledList>
-        </Section>
+        <Flex direction="column" height="100%">
+          <Flex.Item>
+            <Flex direction="row" align="start">
+                {img_url && (
+                  <Flex.Item mr={1}>
+                    <img class="fit-picture" width="192" height="192" src={img_url} style={{ flex: 0 }} />
+                  </Flex.Item>
+                )}
+                <Flex.Item grow={1}>
+                  <TypingScroller key={text} text={text} speed={100}
+                    onTypingStart={() => act("playSound")}
+                    onTypingEnd={() => act("stopSound")}/>
+                </Flex.Item>
+              </Flex>
+            </Flex.Item>
+            <Flex.Item mt={2}>
+              <Flex
+                direction="column"
+                align="center"
+                justify="center"
+                height="100%"
+              >
+                {actions.map((action) => (
+                  <Button
+                    key={action.key}
+                    onClick={() => act(action.key)}
+                    fontSize="16px"
+                    width="200px"
+                    height="40px"
+                    mb={1}
+                  >
+                    {action.text}
+                  </Button>
+                ))}
+              </Flex>
+            </Flex.Item>
+          </Flex>        </Section>
       </Window.Content>
     </Window>
   );
