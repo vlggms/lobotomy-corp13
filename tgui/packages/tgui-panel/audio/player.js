6adcc6198586142e2dad57cdfb31ea6d596810bc
Cupa <5896309+Cupax3@users.noreply.github.com>
2025-07-17 22:29:31 +0200

    Fix: Make Linter Happy

--- tgui/packages/tgui-panel/audio/player.js
+++ tgui/packages/tgui-panel/audio/player.js
@@ -4,85 +4,85 @@
  * @license MIT
  */
 
-import { createLogger } from "tgui/logging"
+import { createLogger } from "tgui/logging";
 
-const logger = createLogger("AudioPlayer")
+const logger = createLogger("AudioPlayer");
 
 export class AudioPlayer {
   constructor() {
-    this.element = null
+    this.element = null;
 
-    this.onPlaySubscribers = []
-    this.onStopSubscribers = []
+    this.onPlaySubscribers = [];
+    this.onStopSubscribers = [];
   }
 
   destroy() {
-    this.element = null
+    this.element = null;
   }
 
   play(url, options = {}) {
     if (this.element) {
-      this.stop()
+      this.stop();
     }
 
-    this.options = options
+    this.options = options;
 
-    const audio = (this.element = new Audio(url))
-    audio.volume = this.volume
-    audio.playbackRate = this.options.pitch || 1
+    const audio = (this.element = new Audio(url));
+    audio.volume = this.volume;
+    audio.playbackRate = this.options.pitch || 1;
 
-    logger.log("playing", url, options)
+    logger.log("playing", url, options);
 
     audio.addEventListener("ended", () => {
-      logger.log("ended")
-      this.stop()
-    })
+      logger.log("ended");
+      this.stop();
+    });
 
     audio.addEventListener("error", error => {
-      logger.log("playback error", error)
-    })
+      logger.log("playback error", error);
+    });
 
     if (this.options.end) {
       audio.addEventListener("timeupdate", () => {
         if (
-          this.options.end &&
-          this.options.end > 0 &&
-          audio.currentTime >= this.options.end
+          this.options.end
+          && this.options.end > 0
+          && audio.currentTime >= this.options.end
         ) {
-          this.stop()
+          this.stop();
         }
-      })
+      });
     }
 
-    audio.play().catch(error => logger.log("playback error", error))
+    audio.play().catch(error => logger.log("playback error", error));
 
-    this.onPlaySubscribers.forEach(subscriber => subscriber())
+    this.onPlaySubscribers.forEach(subscriber => subscriber());
   }
 
   stop() {
-    if (!this.element) return
+    if (!this.element) return;
 
-    logger.log("stopping")
+    logger.log("stopping");
 
-    this.element.pause()
-    this.element = null
+    this.element.pause();
+    this.element = null;
 
-    this.onStopSubscribers.forEach(subscriber => subscriber())
+    this.onStopSubscribers.forEach(subscriber => subscriber());
   }
 
   setVolume(volume) {
-    this.volume = volume
+    this.volume = volume;
 
-    if (!this.element) return
+    if (!this.element) return;
 
-    this.element.volume = volume
+    this.element.volume = volume;
   }
 
   onPlay(subscriber) {
-    this.onPlaySubscribers.push(subscriber)
+    this.onPlaySubscribers.push(subscriber);
   }
 
   onStop(subscriber) {
-    this.onStopSubscribers.push(subscriber)
+    this.onStopSubscribers.push(subscriber);
   }
 }
